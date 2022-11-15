b3cad8eb62c9ecefe36a8c9f7856b15c6d1079a5
Sanjaya Gihan <73861147+sanjayagihan@users.noreply.github.com>
2022-11-16 00:45:07 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,7 @@
 #include<avr/io.h>
 
 int main(){
-    DDRD |= (1<<PD6);                                   // Fart PWM output at OCOA pin
+    DDRD |= (1<<PD6);                                   // Fast PWM output at OCOA pin
     TCNT0 = 0;                                          // Set timer0 to 0
     OCR0A = 127;                                        // Set OCR0A to 127 , set PWM duty cycle to 50%
     TCCR0A |= (1<<COM0A1) | (1<<WGM00) | (1<<WGM01);    // Set fast PWM mode
